52486dffa6ad35aaa3fe2e4b1f5d1d4c98c3bcde
Jay Patel <jayp76557@gmail.com>
2025-02-07 14:53:03 -0500

    index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ console.log("React app is starting...");
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
-// import "./index.css";
 
+import "./index.css";
+import App from "./App.jsx";
 console.log("React app is starting...");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
