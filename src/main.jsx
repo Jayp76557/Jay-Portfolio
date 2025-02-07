cb8de4af70531aa83015de78149cd1b7b7eedbf7
Jay Patel <jayp76557@gmail.com>
2025-02-07 14:49:37 -0500

    console.log("React app is starting...");

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
+console.log("React app is starting...");
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
 
 console.log("React app is starting...");
 ReactDOM.createRoot(document.getElementById("root")).render(
